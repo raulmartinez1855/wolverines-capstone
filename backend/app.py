15faa1af44b2988addf1799aa3851520c1552fe1
Raul Martinez <raulmartinez1855@gmail.com>
2024-04-06 17:06:29 -0400

    mappings endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, jsonify, Response
 from sklearn.dummy import DummyClassifier
 from flask_cors import CORS, cross_origin
+import pandas as pd
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
-#
+df = pd.read_csv("../Data/Final Dataset.csv")
+
+
+def gen_cols_json(cols):
+    return df[cols].drop_duplicates().to_dict(orient="records")
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -18,3 +23,23 @@ def index():
         data = json.loads(request.data)
         clf = DummyClassifier()
         return data
+
+
+@app.route("/mappings", methods=["GET"])
+@cross_origin()
+def get_player_mappings():
+    if request.method == "GET":
+
+        seasons = gen_cols_json(["Season"])
+        players = gen_cols_json(["PlayerId", "Player", "Team"])
+        positions = gen_cols_json(["PositionId", "Position"])
+        teams = gen_cols_json(["TeamId", "Team"])
+        conferences = gen_cols_json(["ConferenceId", "Conference"])
+
+        return jsonify(
+            seasons=seasons,
+            players=players,
+            positions=positions,
+            teams=teams,
+            conferences=conferences,
+        )

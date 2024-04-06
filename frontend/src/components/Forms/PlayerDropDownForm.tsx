1c65369ee78801d534afc9c42b501e1bfdee7909
Raul Martinez <raulmartinez1855@gmail.com>
2024-04-06 17:07:51 -0400

    add player opts, fix performance issue

--- frontend/src/components/Forms/PlayerDropDownForm.tsx
+++ frontend/src/components/Forms/PlayerDropDownForm.tsx
@@ -2,36 +2,17 @@ import { Field } from "formik";
 import * as yup from "yup";
 import FormStateWrapper from "./FormStateWrapper";
 import SelectDropDown from "./SelectDropDown";
+import { DropDownOption } from "./SelectDropDown/drop-down-styles";
 
 const initialPlayerDropDownFormValues = {
-  playerName: "",
+  playerName: undefined,
 };
 
-const selectOpts = [
-  "Darrius Clemons",
-  "Mike Sainristil",
-  "Amorion Walker",
-  "Roman Wilson",
-  "Blake Corum",
-  "Will Johnson",
-  "Fredrick Moore",
-  "Keon Sabb",
-  "Jayden Denegal",
-  "Micah Pollard",
-  "Karmello English",
-  "Josaiah Stewart",
-  "Brandyn Hillman",
-  "Cornelius Johnson",
-  "Donovan Edwards",
-  "Makari Paige",
-  "Derrick Moore",
-  "Tyler Morris",
-  "J.J. McCarthy",
-  "Rod Moore",
-  "Zeke Berry",
-].map((v) => ({ label: v, value: v }));
-
-export default function PlayerDropDownForm() {
+export default function PlayerDropDownForm({
+  playerOptions,
+}: {
+  playerOptions: DropDownOption[];
+}) {
   return (
     <FormStateWrapper
       initialValues={initialPlayerDropDownFormValues}
@@ -39,22 +20,20 @@ export default function PlayerDropDownForm() {
         playerName: yup.string().required("Player Name is required"),
       })}
     >
-      <>
-        <h2 className="text-4xl text-white">
-          Transfer Portal Prediction Model - by Player
-        </h2>
-        <p className="text-xl mb-[6.4rem]">
-          Utilize this model to generate a transfer portal prediction for an
-          individual player by name
-        </p>
-        <h2 className="text-2xl text-white">Search by Player</h2>
-        <Field
-          name="playerName"
-          labelCopy="Select Player"
-          options={selectOpts}
-          component={SelectDropDown}
-        />
-      </>
+      <h2 className="text-4xl text-white">
+        Transfer Portal Prediction Model - by Player
+      </h2>
+      <p className="text-xl mb-[6.4rem]">
+        Utilize this model to generate a transfer portal prediction for an
+        individual player by name
+      </p>
+      <h2 className="text-2xl text-white">Search by Player</h2>
+      <Field
+        name="playerName"
+        labelCopy="Select Player"
+        options={playerOptions}
+        component={SelectDropDown}
+      />
     </FormStateWrapper>
   );
 }

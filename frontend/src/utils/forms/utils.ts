a61505eedaaf27b9d93f9d73b4e8a43c4db2330c
Raul Martinez <raulmartinez1855@gmail.com>
2024-04-06 17:07:33 -0400

    std call for getting dropdown values

--- frontend/src/utils/forms/utils.ts
+++ frontend/src/utils/forms/utils.ts
@@ -1,3 +1,6 @@
+import { DropDownOption } from "@/components/Forms/SelectDropDown/drop-down-styles";
+import axios from "axios";
+
 export const backendUrl = process.env.NEXT_PUBLIC_BACKEND_SERVER as string;
 
 export enum FormSteps {
@@ -8,3 +11,38 @@ export enum FormSteps {
 
 export const loading = (delay: number, value: any) =>
   new Promise((resolve) => setTimeout(resolve, delay, value));
+
+export interface FormDropDownOptionValues {
+  players: DropDownOption[];
+  conferences: DropDownOption[];
+  positions: DropDownOption[];
+  seasons: DropDownOption[];
+  teams: DropDownOption[];
+}
+
+export const getDropDownFormOpts =
+  async (): Promise<FormDropDownOptionValues> => {
+    const res = await axios.get(backendUrl + "/mappings");
+    res.data.players = res.data.players.map((v: any) => ({
+      label: `${v.Player} (${v.Team})`,
+      value: v.PlayerId,
+    }));
+    res.data.conferences = res.data.conferences.map((v: any) => ({
+      label: v.Conference,
+      value: v.ConferenceId,
+    }));
+    res.data.positions = res.data.positions.map((v: any) => ({
+      label: v.Position,
+      value: v.PositionId,
+    }));
+    res.data.seasons = res.data.seasons.map((v: any) => ({
+      label: v.Season,
+      value: v.Season,
+    }));
+    res.data.teams = res.data.teams.map((v: any) => ({
+      label: v.Team,
+      value: v.TeamId,
+    }));
+
+    return res.data;
+  };
